ee6d19f51d718e87e282afe4e4a9bc1756c40c58
Evan Berg <evan.m.berg@gmail.com>
2022-06-01 15:13:05 -0400

    create user and login working. not sure about logout w/jwt, may be more of a client side thing

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  require "securerandom"
+
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :username, presence: true
+end
