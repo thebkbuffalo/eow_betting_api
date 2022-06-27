609dfd45c25560da3925c3c791fd11f6301fc912
Evan Berg <evan.m.berg@gmail.com>
2022-06-27 13:42:29 -0400

    added react site url to CORS policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3001", "http://127.0.0.1:3000", "localhost:3000"
+    origins "localhost:3001", "http://127.0.0.1:3000", "localhost:3000", "https://glistening-paletas-cd167e.netlify.app"
 
     resource "*",
       headers: :any,
